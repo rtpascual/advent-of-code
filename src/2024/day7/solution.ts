418a73c13abde6a3cb91f91f36c39972f72e00e9
Roshane Pascual <roshanepascual@gmail.com>
2024-12-07 00:00:16 -0800

    2024 day 7

---
+++ src/2024/day7/solution.ts
@@ -0,0 +1,110 @@
+import fs from 'fs/promises';
+import path from "path";
+import { fileURLToPath } from "url";
+import { profile } from '../../utils/profile';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+async function getData() {
+  const file = await fs.readFile(path.join(__dirname, "input.txt"));
+  const data = file.toString().split('\n');
+  return data;
+}
+
+async function part1() {
+  const data = await getData();
+  let total = 0;
+
+  for (let i = 0; i < data.length; i++) {
+    const { testValue, numbers } = getValues(data[i]);
+    const operatorPermutations = generateOperatorPermutations(numbers.length - 1);
+
+    for (const ops of operatorPermutations) {
+      if (evaluate(numbers, ops) === testValue) {
+        total += testValue;
+        break;
+      }
+    }
+  }
+
+  return total;
+}
+
+async function part2() {
+  const data = await getData();
+  let total = 0;
+
+  for (let i = 0; i < data.length; i++) {
+    const { testValue, numbers } = getValues(data[i]);
+    const operatorPermutations = generateOperatorPermutations(numbers.length - 1, true);
+
+    for (const ops of operatorPermutations) {
+      if (evaluate(numbers, ops) === testValue) {
+        total += testValue;
+        break;
+      }
+    }
+  }
+
+  return total;
+}
+
+function getValues(equation: string) {
+  const [ testValueString , numberString ] = equation.split(':');
+  const testValue = Number(testValueString);
+  const numbers = numberString.trim().split(' ').map(Number);
+
+  return {
+    testValue,
+    numbers
+  }
+}
+
+function generateOperatorPermutations(length: number, allowConcatenation: boolean = false): string[][] {
+  const operators = ['+', '*'];
+  const result: string[][] = [];
+
+  if (allowConcatenation) {
+    operators.push('||');
+  }
+
+  function generate(current: string[]) {
+    if (current.length === length) {
+      result.push([...current]);
+      return;
+    }
+    for (const op of operators) {
+      current.push(op);
+      generate(current);
+      current.pop();
+    }
+  }
+
+  generate([]);
+
+  return result;
+}
+
+function evaluate(numbers: number[], operators: string[]): number {
+  let result = numbers[0];
+
+  for (let i = 0; i < operators.length; i++) {
+    if (operators[i] === '+') {
+      result += numbers[i + 1];
+    } else if (operators[i] === '*') {
+      result *= numbers[i + 1];
+    } else if (operators[i] === '||') {
+      result = Number(`${result}${numbers[i + 1]}`);
+    }
+  }
+
+  return result;
+}
+
+profile("part 1", async () => {
+  console.log(await part1());
+});
+
+profile("part 2", async () => {
+  console.log(await part2());
+});
